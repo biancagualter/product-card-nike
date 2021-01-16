5c19f5c01409c9f544a0a4e5058f4dcbc2f342f4
Bianca Gualter <bncgualter@gmail.com>
2021-01-16 14:06:14 -0300

    Adicionando dinamismo

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,28 @@ const showMenu = (toggleId, navId) => {
 
 showMenu('nav-toggle', 'nav-menu')
 
+const sizes = document.querySelectorAll('.sizes')
+const colors = document.querySelectorAll('.sneaker_color')
+const sneaker = document.querySelectorAll('.sneaker_img')
+
+function changeSize() {
+    sizes.forEach(size => size.classList.remove('active'))
+    this.classList.add('active')
+}
+
+function changeColor() {
+    let primary = this.getAttribute('primary')
+    let color = this.getAttribute('color')
+    let sneakerColor = document.querySelector(`.sneaker_img[color="${color}"]`)
+
+    colors.forEach(c => c.classList.remove('active'))
+    this.classList.add('active')
+
+    document.documentElement.style.setProperty('--primary', primary)
+
+    sneaker.forEach(s => s.classList.remove('shows'))
+    sneakerColor.classList.add('shows')
+}
+
+sizes.forEach(size => size.addEventListener('click', changeSize))
+colors.forEach(c => c.addEventListener('click', changeColor))
